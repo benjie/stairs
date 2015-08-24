e149178c4315cc248bf4c3d1739e1c78445cb81e
Benjie Gillam <benjie@jemjie.com>
2015-08-24 19:54:26 +0100

    Safer bootup with wifi abort.

--- init.lua
+++ init.lua
@@ -1,8 +1,24 @@
 boot = true
+
 function start()
   if boot then
     dofile("main.lua")
+  else
+    print("Aborted boot before main")
   end
 end
 
-tmr.alarm(0, 30000, 0, start)
+function remote()
+  if boot then
+    -- Connect to wifi
+    dofile('wifi.lua')
+
+    -- Set up telnet server
+    dofile('telnet.lua')
+    tmr.alarm(0, 15000, 0, start)
+  else
+    print("Aborted boot before remote setup")
+  end
+end
+
+tmr.alarm(0, 5000, 0, remote)
--- main.lua
+++ main.lua
@@ -3,11 +3,6 @@ dofile('mcp23017.lua')
 
 -- Load LED helpers
 dofile('leds.lua')
+
 -- Start linear animation
 led_animate_bounce()
-
--- Connect to wifi
-dofile('wifi.lua')
-
--- Set up telnet server
-dofile('telnet.lua')
